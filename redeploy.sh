80b5daece46f118602c329b69033eb5f9fddb597
Meer Sawood <msawood@redhat.com>
2023-08-30 12:20:27 +0530

    added sha based deployment

--- redeploy.sh
+++ redeploy.sh
@@ -4,4 +4,9 @@ set -ex
 #nginx
 git pull origin main
 
+
+COMMIT_SHA=$(git rev-parse HEAD)
+sed -i "s/\${IMAGE_TAG}/$COMMIT_SHA/g" docker-compose.yml
+
+
 docker compose down && docker compose pull &&  docker compose up -d 
